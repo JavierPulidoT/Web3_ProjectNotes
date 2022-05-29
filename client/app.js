c4901f5da43060cba0493e14aa25a1ff76f050c6
JavierPulidoT <javierp.pulido@gmail.com>
2022-05-28 23:48:00 -0500

    Load and Create Task/Notes/idea

--- client/app.js
+++ client/app.js
@@ -1,10 +1,13 @@
 App= {
 
     web3Provider: '',
+    contracts: {},
+    //account
 
     init: () => {
         console.log('Loaded')
         App.loadEthereum()
+        App.loadAccount()
         App.loadContracts()
     },
     // Function para ver si existe el obj ethereum 
@@ -14,7 +17,7 @@ App= {
             App.web3Provider = window.ethereum
             console.log('ethereum existe')
             // Requerir address de la cuenta  (Conexion) modo nuevo Metamask
-            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            await window.ethereum.request({ method: 'eth_requestAccounts'})
 
         }else if (window.web3) 
             { 
@@ -26,11 +29,33 @@ App= {
         }
     },
 
+    // Cargar Cuenta
+    loadAccount: async () => {
+           const accounts =  await window.ethereum.request({ method: 'eth_requestAccounts' })
+           App.account = accounts[0]
+
+           console.log(accounts)
+    },
+
     // Cargar contratos
     loadContracts: async () => {
         const res = await fetch("TasksContract.json")
         const tasksContractJSON = await res.json()
-        console.log(tasksContractJSON)
+        
+        App.contracts.tasksContract = TruffleContract(tasksContractJSON)
+
+        App.contracts.tasksContract.setProvider(App.web3Provider) // Contrato conectando desde nuestra cuenta Metamask
+
+        //Contrato finalmente configurado
+        App.tasksContract = await App.contracts.tasksContract.deployed()
+    },
+
+    // Crear Tarea
+    createTask: async (title, description) => {
+        const result = await App.tasksContract.createTask(title, description, {
+            from: App.account
+        });
+        console.log(result.logs[0].args);
     }
 }
 
